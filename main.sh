7df02e7764d0fca1b8cf5a521a2852c80825de22
HardDie <oleg1995sysoev@yandex.ru>
2016-04-02 13:00:25 +0600

    Лабораторная 2. Создание папок с возможностью выбора месторасположения папок их количества и имени

--- main.sh
+++ main.sh
@@ -5,38 +5,46 @@ echo
 echo -n "Введите директорию в которой создавать папки: "
 
 read mainDir
-firstLevel=5
-secondLevel=10
-thirdLevel=20
 
+echo -n "Введите количество папок на первом уровне: "
+read firstLevel
+echo -n "Введите их имя: "
+read nameFirst
 
+echo -n "Введите количество папок на втором уровне: "
+read secondLevel
+echo -n "Введите их имя: "
+read nameSecond
 
-k=0
+echo -n "Введите количество файлов: "
+read thirdLevel
+echo -n "Введите их имя: "
+read nameThird
 
 if [ -d $mainDir ]; then
 	cd $mainDir
 	i=0	#Инициализация переменной цикла
 	while [ $i -lt $firstLevel  ]; do
 
-		mkdir "folder$i"
+		mkdir "$nameFirst$i"
 		if [ $? -eq 1 ]; then
 			echo "Не удалось создать папку \"folder$i\" в директории \"$PWD\""
 			exit 1
 		fi
-		cd "folder$i"
+		cd "$nameFirst$i"
 		j=0	#Инициализация переменной цикла
 		while [ $j -lt $secondLevel ]; do
 
-			mkdir "folder$j"
+			mkdir "$nameSecond$j"
 			if [ $? -eq 1 ]; then
 				echo "Не удалось создать папку \"folder$i$j\" в директории \"$PWD\""
 				exit 1
 			fi
-			cd "folder$j"
+			cd "$nameSecond$j"
 			k=0	#Инициализация переменной цикла
 			while [ $k -lt $thirdLevel ]; do
 
-				touch "file$k"
+				touch "$nameThird$k"
 				k=$(($k + 1))
 
 			done
