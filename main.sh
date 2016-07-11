0aa7db5bbfa4b03aacee579029fca7830e2b730a
HardDie <oleg1995sysoev@yandex.ru>
2016-07-11 09:58:25 +0600

    Слежение за процессом

--- main.sh
+++ main.sh
@@ -1,136 +1,151 @@
 #!/bin/bash
 
-alarmCount() {
-	return `crontab -l | grep \#alarm | wc -l`
+Menu() {
+	clear;
+	echo "			Bash lab №4, Process control";
+	echo "--------------------[Main menu]--------------------";
+	echo "1 - Add process control";
+	echo "2 - Delete process control";
+	echo "3 - Show list";
+	echo "4 - Exit";
+	echo;
+	echo -n "Choose point menu: ";
+
+	Input 1 4;
+	return "$?";
 }
 
-menu() {
-	echo "--------------------[Главное меню]--------------------"
-	echo "1 - Добавить будильник"
-	echo "2 - Посмотреть список будильников"
-	echo "3 - Удалить будильник"
-	echo "4 - Выход"
-	echo
-	echo -n "Выберите пункт меню: "
-
-	pointMenu=0
-	while [ $pointMenu -eq 0 ]; do
-		read pointMenu
-		case "$pointMenu" in
-			1)
-				pointMenu=1;;
-			2)
-				pointMenu=2;;
-			3)
-				pointMenu=3;;
-			4)
-				pointMenu=4;;
-			*)
-				echo -n "Неверный ввоод, повторите попытку: "
-				pointMenu=0;;
-		esac
-	done
-	return "$pointMenu"
-}
-
-input() {
-	temp=-1
+Input() {
+	temp=-1;
 	while [[ $temp -eq -1 ]]; do
-		read temp
+		read temp;
 		case "$temp" in
 			[0-9] | [0-9][0-9])
 				if [[ $temp -lt $1 || $temp -gt $2 ]]; then
-					echo -n "Неверный ввод, повторите попытку: "
-					temp=-1
+					echo -n "Wrong input, try again: ";
+					temp=-1;
 				fi;;
 			*)
-				echo -n "Неверный ввод, повторите попытку: "
+				echo -n "Wrong input, try again: ";
 				temp=-1;;
 		esac
 	done
-	return "$temp"
+	return "$temp";
 }
 
-addAlarm() {
-	echo "--------------------[Добавить будильник]--------------------"
-	echo -n "Введите час: "
-	input 0 23
-	hour=$?
+AddProcess() {
+	clear;
+	echo "--------------------[Add process control]--------------------";
+	echo "1 - Pereodicity minutes";
+	echo "2 - Pereodicity hours";
+	echo "3 - Back";
+	echo;
+	echo -n "Choose menu point: ";
+	Input 1 3;
 
-	echo -n "Введите минуты: "
-	input 0 59
-	minute=$?
-
-	echo -n "Введите текст сообщения: "
-	read message
-
-	tmp_val=$$
-	crontab -l > .tmp.$tmp_val
-	echo "$minute $hour * * * export DISPLAY=:0 && xterm -e "dialog --msgbox \'$message\' 234 234" #alarm" >> .tmp.$tmp_val
-	crontab .tmp.$tmp_val
-	rm .tmp.$tmp_val
-}
-
-showAlarm() {
-	echo "--------------------[Показать список будильников]--------------------"
-	alarms=$(crontab -l | awk -F# ' /#alarm/ { print $1 } ' | awk -F"'" '{ print $1, " |", $2 }' |  awk ' { gsub("export DISPLAY=:0 && xterm -e dialog --msgbox","",$0); gsub("*","",$0); gsub("  ","",$0); printf "%2d %s %s|%s\n", NR, $2, $1, $0 } ' | awk -F"|" '{ print $1, $3 }')
-	echo | awk ' {print "==============================="; print " №  H  M  Message"; print "===============================";} '
-	echo "$alarms"
-	return 0
-}
-
-deleteAlarm() {
-	echo "--------------------[Удалить будильник]--------------------"
-	alarmCount	# Вызываем функция для подсчета текущего количества будильников
-	cntAlarm=$?
-	if [ $cntAlarm -eq 0 ]; then
-		echo "Нет будильников"
-		return -1
-	fi
-	echo -n "Введите номер будильника: "
-	input 1 $cntAlarm	# Считываем номер удаляемого будильника, от 1 до количества будильников
-	numberAlarm=$?	# Сохраняем номер удаляемого будильника
-	temp=$$
-	touch .temp.$temp
-	crontab -l > .temp.$temp
-	tmp=`cat .temp.$temp | grep alarm -n | awk -F: '{ print $1 }'`
-	j=1
-	for i in $tmp
-	do
-		if [ $j -eq $numberAlarm ]; then
-			strNum=$i
-		fi
-		j=$(($j+1))
-	done
-	sed -i "${strNum}d" .temp.$temp
-	crontab .temp.$temp
-	rm .temp.$temp
-	return 0
-}
-
-clear
-clear
-clear
-echo "			Bash лабораторная №3, Будильник с crontab"
-echo
-
-isDone=0
-while [[ $isDone -ne 1 ]]; do
-	menu
 	case "$?" in
 		1)
-			addAlarm
+			echo -n "Every ... minutes: ";
+			Input 1 30;
+			per=$?;
+			echo -n "Input process name: ";
+			read process;
+			tmp_val=$$;
+			touch .tmp.$tmp_val;
+			crontab -l > .tmp.$tmp_val;
+			echo "*/$per * * * * /home/hard_die/school/linux/4_process_control/main.sh $process #process_control" >> .tmp.$tmp_val;
+			crontab .tmp.$tmp_val;
+			rm .tmp.$tmp_val;
 			;;
 		2)
-			showAlarm
+			echo -n "Every ... hours: ";
+			Input 1 30;
+			per=$?;
+			echo -n "Input process name: ";
+			read process;
+			tmp_val=$$;
+			touch .tmp.$tmp_val;
+			crontab -l > .tmp.$tmp_val;
+			echo "0 */$per * * * /home/hard_die/school/linux/4_process_control/main.sh $process #process_control" >> .tmp.$tmp_val;
+			crontab .tmp.$tmp_val;
+			rm .tmp.$tmp_val;
 			;;
 		3)
-			deleteAlarm
-			;;
-		4)
-			isDone=1
+			return 0;
 			;;
 	esac
-done
+
+	return 0;
+}
+
+DeleteProcess() {
+	clear;
+	ProcessList;
+	echo "--------------------[Delete process control]--------------------";
+	echo;
+	echo -n "Choose process for delete: ";
+	processCount=`crontab -l | grep \#process_control | wc -l`;
+	if [[ $processCount -eq 0 ]]; then
+		clear;
+		echo "No process control"
+		echo -n "Press any key...";
+		read;
+		return 0;
+	fi
+	Input 1 $processCount;
+	processForDelete=$?;
+	processList=`crontab -l | grep \#process_control -n | awk -F: '{ print $1 }'`;
+	tmp_val=$$;
+	touch .tmp.$tmp_val;
+	crontab -l > .tmp.$tmp_val;
+	j=1;
+	for i in $processList
+	do
+		if [[ $j -eq $processForDelete ]]; then
+			sed -i "${i}d" .tmp.$tmp_val;
+		fi
+		j=$(($j+1));
+	done
+	crontab .tmp.$tmp_val;
+	rm .tmp.$tmp_val;
+	return 0;
+}
+
+ProcessList() {
+	echo " №    hour  min    process";
+	crontab -l | grep \#process_control | awk -F# '{ print $1 }' | awk '{ printf "%2d) %5s %5s    %s\n", NR, $2, $1, $7 }';
+	return 0;
+}
+
+if [[ $# -eq 1 ]]; then
+	echo "Hello" >> /home/hard_die/school/linux/4_process_control/tmp;
+	if [ `ps -ax | grep $1 | wc -l` -eq 1 ]; then
+		echo "Hello" >> /home/hard_die/school/linux/4_process_control/tmp;
+	fi
+else
+	isDone=0;
+	while [[ $isDone -ne 1 ]]; do
+		Menu;
+		case "$?" in
+			1)
+				AddProcess;
+				;;
+			2)
+				DeleteProcess;
+				;;
+			3)
+				clear;
+				echo "--------------------[Process list]--------------------";
+				ProcessList;
+				echo -n "Press any key...";
+				read;
+				;;
+			4)
+				isDone=1
+				clear;
+				;;
+		esac
+	done
+fi
 
 exit 0
