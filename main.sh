8a536fe6e7398dd75eccb2bb20748afdee60b523
HardDie <oleg1995sysoev@yandex.ru>
2016-07-14 10:36:37 +0600

    Добавлены исправления

--- main.sh
+++ main.sh
@@ -1,186 +1,59 @@
 #!/bin/bash
 
-Menu() {
-	clear;
-	echo "			Bash lab №5, Log";
-	echo "--------------------[Main menu]--------------------";
-	echo "1 - Show log";
-	echo "2 - Exit";
-	echo;
-	echo -n "Choose point menu: ";
+if [[ $# -eq 1 ]]; then
+	#====================
+	# Start
+	#
+	#	Запуск монтирования
+	#====================
+	if [[ $1 = "start" ]];then
+		temp=`df -h | grep "^192.168.1.2:/home/shared.*/media/server" | wc -l`;
+		if [[ $temp -eq 0 ]]; then
+			sudo mount 192.168.1.2:/home/shared /media/server;
+		fi
 
-	Input 1 2;
-	return "$?";
-}
+		temp=`df -h | grep "^192.168.1.2:/home/shared.*/media/server" | wc -l`;
+		if [[ $temp -eq 0 ]]; then
+			echo "Error mount /home/shared in /media/server" >> /home/harddie/error_mount.log;
+			exit;
+		fi
 
-Input() {
-	temp=-1;
-	while [[ $temp -eq -1 ]]; do
-		read temp;
-		case "$temp" in
-			[0-9] | [0-9][0-9])
-				if [[ $temp -lt $1 || $temp -gt $2 ]]; then
-					echo -n "Wrong input, try again: ";
-					temp=-1;
-				fi;;
-			*)
-				echo -n "Wrong input, try again: ";
-				temp=-1;;
-		esac
-	done
-	return "$temp";
-}
+		cd /media/server;
+		log="main.sh.log";
+		ip=`ifconfig eth0 | grep "inet addr:" | awk '{ print $2 }' | awk -F: '{ print $2 }'`;
 
-ProcessList() {
-	echo " №    hour  min    process";
-	crontab -l | grep \#process_control | awk -F# '{ print $1 }' | awk '{ printf "%2d) %5s %5s    %s\n", NR, $2, $1, $7 }';
-	return 0;
-}
+		echo >> $log;
+		echo "[START]" >> $log;
+		echo "Name: `whoami`" >> $log;
+		echo "IP: $ip" >> $log;
+		echo "Start: `date -R`" >> $log;
+		echo >> $log;
 
-CheckTime() {
-	tmp=`echo $1 | awk -F: {'print $1'}`;
-	case "$tmp" in
-		[0-9][0-9])
-		 	if [[ $tmp -lt 0 || $tmp -gt 23 ]]; then
-				return 1;
-			fi
-			;;
-		*)
-			return 1;
-			;;
-	esac
-
-	tmp=`echo $1 | awk -F: {'print $2'}`;
-	case "$tmp" in
-		[0-9][0-9])
-		 	if [[ $tmp -lt 0 || $tmp -gt 59 ]]; then
-				return 1;
-			fi
-			;;
-		*)
-			return 1;
-			;;
-	esac
-
-	tmp=`echo $1 | awk -F: {'print $3'}`;
-	case "$tmp" in
-		[0-9][0-9])
-		 	if [[ $tmp -lt 0 || $tmp -gt 59 ]]; then
-				return 1;
-			fi
-			;;
-		*)
-			return 1;
-			;;
-	esac
-
-	return 0;
-}
-
-InputDate() {
-	result=1;
-	while [[ $result -eq 1 ]]
-	do
-		echo -n "Input time start(hh:mm:ss)";
-		read timeStart;
-		CheckTime $timeStart;
-		result=$?;
-	done
-
-	result=1;
-	while [[ $result -eq 1 ]]
-	do
-		echo -n "Input time end(hh:mm:ss)";
-		read timeEnd;
-		CheckTime $timeEnd;
-		result=$?;
-	done
-}
-
-ShowLog() {
-	echo -n "Input program name: ";
-	read program;
-	InputDate;
-	timeStart=`date --date="$timeStart" +"%s"`;
-	timeEnd=`date --date="$timeEnd" +"%s"`;
-
-	cd /var/log
-	if [[ -f $program.log ]]; then
-		echo "$program.log";
-		temp=`cat $program.log | wc -l`;
-		j=0;
-		while [[ $j -lt $temp ]]
-		do
-			j=$(($j+1));
-			str=`sed -ne "$j"p $program.log`;
-			tmp=`echo $str | egrep -o "[0-2][0-9]:[0-9][0-9]:[0-9][0-9] "`;
-			date=`date --date="$tmp" +"%s"`;
-			if [[ $date -ge $timeStart && $date -le $timeEnd ]]; then
-				echo "$str";
-			fi
-		done
-	elif [[ -d $program ]]; then
-		echo "Directory";
-		cd $program;
-		for i in `ls`
-		do
-			echo;
-			echo "file: $i";
-			echo;
-			temp=`cat $i | wc -l`;
-			j=0;
-			while [[ $j -lt $temp ]]
-			do
-				j=$(($j+1));
-				str=`sed -ne "$j"p $i`;
-				tmp=`echo $str | egrep -o "[0-2][0-9]:[0-9][0-9]:[0-9][0-9] "`;
-				date=`date --date="$tmp" +"%s"`;
-				if [[ $date -ge $timeStart && $date -le $timeEnd ]]; then
-					echo "$str";
-				fi
-			done
-		done
-	else
-		echo "Syslog"
-		for i in `ls | grep syslog | grep -v gz`
-		do
-			echo;
-			echo "file: $i";
-			echo;
-			temp=`cat $i | wc -l`;
-			j=0;
-			while [[ $j -lt $temp ]]
-			do
-				j=$(($j+1));
-				str=`sed -ne "$j"p $i`;
-				check=`echo $str | grep $program`;
-				if [[ -n $check ]]; then
-					tmp=`echo $str | egrep -o "[0-2][0-9]:[0-9][0-9]:[0-9][0-9] "`;
-					date=`date --date="$tmp" +"%s"`;
-					if [[ $date -ge $timeStart && $date -le $timeEnd ]]; then
-						echo "$str";
-					fi
-				fi
-			done
-		done
+	#====================
+	# Stop
+	#
+	#	Остановка монтирования
+	#====================
+	elif [[ $1 = "stop" ]]; then
+		temp=`df -h | grep "^192.168.1.2:/home/shared.*/media/server" | wc -l`;
+		if [[ $temp -ne 0 ]]; then
+			cd /media/server;
+			log="main.sh.log";
+			ip=`ifconfig eth0 | grep "inet addr:" | awk '{ print $2 }' | awk -F: '{ print $2 }'`;
+			echo >> $log;
+			echo "[STOP]" >> $log;
+			echo "Name: `whoami`" >> $log;
+			echo "IP: $ip" >> $log;
+			echo "Stop: `date -R`" >> $log;
+			echo "Uptime: `uptime -p`" >> $log;
+			echo >> $log;
+			#sleep 2;
+			sudo umount -l /media/server;
+		fi
 	fi
-	#cat error_log | egrep -o "[0-9][0-9]:[0-9][0-9]:[0-9][0-9] ";
-	echo "End, press any key...";
-	read;
-}
+else
+	echo "Usage $0 [start, stop]";
+fi
 
-isDone=0;
-while [[ $isDone -ne 1 ]]; do
-	Menu;
-	case "$?" in
-		1)
-			ShowLog;
-			;;
-		2)
-			isDone=1
-			clear;
-			;;
-	esac
-done
 
 exit 0
