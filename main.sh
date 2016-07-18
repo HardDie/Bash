143ccbbcb61184e6736593bafc12a2abf15a8316
HardDie <oleg1995sysoev@yandex.ru>
2016-07-18 13:25:53 +0600

    Lab 7

--- main.sh
+++ main.sh
@@ -1,59 +1,7 @@
 #!/bin/bash
 
-if [[ $# -eq 1 ]]; then
-	#====================
-	# Start
-	#
-	#	Запуск монтирования
-	#====================
-	if [[ $1 = "start" ]];then
-		temp=`df -h | grep "^192.168.1.2:/home/shared.*/media/server" | wc -l`;
-		if [[ $temp -eq 0 ]]; then
-			sudo mount 192.168.1.2:/home/shared /media/server;
-		else
-			exit;
-		fi
-
-		temp=`df -h | grep "^192.168.1.2:/home/shared.*/media/server" | wc -l`;
-		if [[ $temp -eq 0 ]]; then
-			echo "Error mount /home/shared in /media/server" >> /home/harddie/error_mount.log;
-			exit;
-		fi
-
-		log="/media/server/main.sh.log";
-		ip=`ifconfig eth0 | grep "inet addr:" | awk '{ print $2 }' | awk -F: '{ print $2 }'`;
-
-		echo >> $log;
-		echo "[START]" >> $log;
-		echo "Name: `whoami`" >> $log;
-		echo "IP: $ip" >> $log;
-		echo "Start: `date -R`" >> $log;
-		echo >> $log;
-
-	#====================
-	# Stop
-	#
-	#	Остановка монтирования
-	#====================
-	elif [[ $1 = "stop" ]]; then
-		temp=`df -h | grep "^192.168.1.2:/home/shared.*/media/server" | wc -l`;
-		if [[ $temp -ne 0 ]]; then
-			log="/media/server/main.sh.log";
-			ip=`ifconfig eth0 | grep "inet addr:" | awk '{ print $2 }' | awk -F: '{ print $2 }'`;
-			echo >> $log;
-			echo "[STOP]" >> $log;
-			echo "Name: `whoami`" >> $log;
-			echo "IP: $ip" >> $log;
-			echo "Stop: `date -R`" >> $log;
-			echo "Uptime: `uptime -p`" >> $log;
-			echo >> $log;
-			#sleep 2;
-			sudo umount -l /media/server;
-		fi
-	fi
-else
-	echo "Usage $0 [start, stop]";
-fi
+clear;
+echo "		Lab 7. Firewall";
 
 
 exit 0
