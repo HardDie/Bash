843f7d1e3b37eaff0fcc13ddc6cb6fda1aaeaa95
HardDie <oleg1995sysoev@yandex.ru>
2016-07-25 12:32:18 +0700

    Реализован backup файлов и папок

--- main.sh
+++ main.sh
@@ -42,11 +42,16 @@ SetSource() {
 	clear;
 	echo -n "Set source path: ";
 	read srcPath;
-	if [[ !( -d $srcPath ) && !( -f $srcPath ) ]]; then
+	if [[ !( -d $srcPath ) && !( -f $srcPath ) && !( -b $srcPath ) ]]; then
 		echo "Error. Wrong source path.";
 		srcPath="None";
 		read;
 		return 0;
+	fi
+
+	endChar=`echo $srcPath | awk -F'/' '{ printf"%s\n", substr($0,length,1) }'`;
+	if [[ !($endChar = '/') && (-d $srcPath) ]]; then
+		srcPath="$srcPath/";
 	fi
 }
 
@@ -54,17 +59,21 @@ SetDestination() {
 	clear;
 	echo -n "Set destination path: ";
 	read dstPath;
-	if [[ -f $dstPath ]]; then
+	if [[ !( -e $dstPath ) ]]; then
+		echo "Error. Wrong destination path.";
+		dstPath="None";
+		read;
+		return 0;
+	elif [[ !( -d $dstPath ) ]]; then
 		echo "Error. Destination path should be a folder.";
 		dstPath="None";
 		read;
 		return 0;
 	fi
-	if [[ !( -d $dstPath ) ]]; then
-		echo "Error. Wrong destination path.";
-		dstPath="None";
-		read;
-		return 0;
+
+	endChar=`echo $dstPath | awk -F'/' '{ printf"%s\n", substr($0,length,1) }'`;
+	if [[ !($endChar = '/') ]]; then
+		dstPath="$dstPath/";
 	fi
 }
 
@@ -72,10 +81,44 @@ Backup() {
 	clear;
 	echo "Backup...";
 	if [[ $source = "None" || $destination = "None" ]]; then
-		echo "Error. Setup source and destination path";
+		echo "Error. Setup source and destination path not set.";
 		read;
 		return 0;
 	fi
+
+	#
+	#	File
+	#
+	if [[ -f $srcPath ]]; then
+		archName=`echo $srcPath | awk -F'/' '{ print $NF }'`;
+		`tar -P -czf $dstPath$archName.tar.gz $srcPath`;
+
+	#
+	#	Folder
+	#
+	elif [[ -d $srcPath ]]; then
+		endChar=`echo $srcPath | awk -F'/' '{ printf"%s\n", substr($0,length,1) }'`;
+		if [[ $endChar = '/' ]]; then
+			archName=`echo $srcPath | awk -F'/' '{ print $(NF-1) }'`;
+		else
+			archName=`echo $srcPath | awk -F'/' '{ print $NF }'`;
+		fi
+		`tar -P -czf $dstPath$archName.tar.gz $srcPath`;
+
+	#
+	#	Block device
+	#
+	elif [[ -b $srcPath ]]; then
+		echo "Source block device: $srcPath";
+		archName=`echo $srcPath | awk -F'/' '{ print $NF }'`;
+		echo "Archive path: $dstPath$archName.tar.gz";
+
+	else
+		echo "Error. Wrong source path";
+	fi
+	
+	echo "Backup end, press any key...";
+	read;
 }
 
 isDone=0;
