3771734854c15d76ef631ebae1c3436fc07db87c
HardDie <oleg1995sysoev@yandex.ru>
2016-07-25 10:10:09 +0700

    Написано главное меню

--- main.sh
+++ main.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-clear;
-echo "		Lab 7. Firewall";
+source="None";
+destination="None";
 
+Menu() {
+	echo "		Lab 8. Backup";
+	echo;
+	echo;
+	echo "1. Set source";
+	echo "2. Set destination";
+	echo "3. Backup";
+	echo "4. Exit";
+}
+
+clear;
+
+Menu;
 
 exit 0
