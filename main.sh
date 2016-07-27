1f690f7cf97101c72721bd790a696527f5a44d5e
HardDie <oleg1995sysoev@yandex.ru>
2016-07-27 10:47:49 +0700

    Реализован перевод из бинарного массива в IP адресс Из IP в бинарный массив Ввод маски в виде количества бит и адресса

--- main.sh
+++ main.sh
@@ -89,7 +89,131 @@ CheckIp() {
 SetMask() {
 	clear;
 	echo "Set mask";
-	read;
+	echo;
+	echo -n "Enter mask: ";
+	read mask;
+	CheckMask $mask;
+	res=$?;
+	if [[ $res -eq 1 ]]; then
+		echo "Wrong mask";
+		mask="None";
+		read;
+	elif [[ $res -eq 2 ]]; then
+		for (( i=1; i <= 32; i++ ))
+		do
+			if [[ $i -le $mask ]]; then
+				mas[$i]=1;
+			else
+				mas[$i]=0;
+			fi
+		done
+		BinToDec;
+		mask=$address;
+	fi
+}
+
+CheckMask() {
+	temp1=`echo $1 | egrep '^[0-9]{1,2}$'`;
+	temp2=`echo $1 | egrep '^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$'`;
+	if [[ -z $temp1 && -z $temp2 ]]; then
+		return 1;
+	fi
+
+	if [[ -n $temp1 ]]; then
+			# Проверка значения на максимальную величину
+		if [[ $temp1 -lt 0 || $temp1 -gt 32 ]]; then
+			return 1;
+		else
+			return 2;
+		fi
+	elif [[ -n $temp2 ]];then
+			# Проверка первого значения
+		temp2=`echo $1 | awk -F'\.' '{ print $1 }'`;
+		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
+			return 1;
+		fi
+
+			# Проверка второго значения
+		temp2=`echo $1 | awk -F'\.' '{ print $2 }'`;
+		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
+			return 1;
+		fi
+
+			# Проверка третьего значения
+		temp2=`echo $1 | awk -F'\.' '{ print $3 }'`;
+		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
+			return 1;
+		fi
+
+			# Проверка четвертого значения
+		temp2=`echo $1 | awk -F'\.' '{ print $4 }'`;
+		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
+			return 1;
+		fi
+	fi
+	return 0;
+}
+
+BinToDec() {
+	for (( i=0; i < 4; i++ ))
+	do
+		val=0;
+		for (( j=0; j < 8; j++ ))
+		do
+			if [[ ${mas[$(( 1 + $j + ( 8 * $i ) ))]} -eq 1 ]]; then
+				case "$j" in
+					0)
+						val=$(( $val + 128 ));
+						;;
+					1)
+						val=$(( $val + 64 ));
+						;;
+					2)
+						val=$(( $val + 32 ));
+						;;
+					3)
+						val=$(( $val + 16 ));
+						;;
+					4)
+						val=$(( $val + 8 ));
+						;;
+					5)
+						val=$(( $val + 4 ));
+						;;
+					6)
+						val=$(( $val + 2 ));
+						;;
+					7)
+						val=$(( $val + 1 ));
+						;;
+				esac
+			fi
+		done
+		if [[ $i -eq 0 ]]; then
+			address=$val;
+		else
+			address=$address.$val;
+		fi
+	done
+}
+
+DecToBin() {
+	temp[0]=`echo $1 | awk -F'\.' '{print $1}'`;
+	temp[1]=`echo $1 | awk -F'\.' '{print $2}'`;
+	temp[2]=`echo $1 | awk -F'\.' '{print $3}'`;
+	temp[3]=`echo $1 | awk -F'\.' '{print $4}'`;
+	for (( i=3; i >= 0; i-- ))
+	do
+		for (( j=0; j < 8; j++ ))
+		do
+			if [[ $((${temp[$i]} % 2)) -eq 0 ]]; then
+				mas[$(( 32 - $j - ( 8 * ( 3 - $i ) ) ))]=0;
+			else
+				mas[$(( 32 - $j - ( 8 * ( 3 - $i ) ) ))]=1;
+			fi
+			temp[$i]=$((${temp[$i]} / 2));
+		done
+	done
 }
 
 Calculate() {
