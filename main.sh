dcd255703fbdab1c69d053038418490c6fd3819c
HardDie <oleg1995sysoev@yandex.ru>
2016-08-15 13:32:27 +0700

    Полностью реализован функционал

--- main.sh
+++ main.sh
@@ -2,6 +2,9 @@
 
 ip="None";
 mask="None";
+available=0;
+masIP;
+masMAC;
 
 Input() {
 	temp=-1;
@@ -23,262 +26,158 @@ Input() {
 
 Menu() {
 	clear;
-	echo "		Lab 9. IP Calculator";
+	echo "		Lab 11. Find hosts";
 	echo;
-	echo "IP   : $ip";
-	echo "Mask : $mask";
-	echo;
-	echo "1. Set ip";
-	echo "2. Set mask";
-	echo "3. Calculate";
-	echo "4. Exit";
+	echo "1. Find hosts";
+	echo "2. Show prev hosts";
+	echo "3. Exit";
 	echo;
 	echo -n "Choose point menu: ";
-	Input 1 4;
+	Input 1 3;
 	return $?;
 }
 
-SetIp() {
+MinHost() {
+	oct1=`echo "$ip" | awk -F \. '{ printf $1 }'`;
+	oct2=`echo "$ip" | awk -F \. '{ printf $2 }'`;
+	oct3=`echo "$ip" | awk -F \. '{ printf $3 }'`;
+	oct4=`echo "$ip" | awk -F \. '{ printf $4 }'`;
+	msk1=`echo "$mask" | awk -F \. '{ printf $1 }'`;
+	msk2=`echo "$mask" | awk -F \. '{ printf $2 }'`;
+	msk3=`echo "$mask" | awk -F \. '{ printf $3 }'`;
+	msk4=`echo "$mask" | awk -F \. '{ printf $4 }'`;
+	ipMin[1]=`echo $(( $oct1 & $msk1 ))`;
+	ipMin[2]=`echo $(( $oct2 & $msk2 ))`;
+	ipMin[3]=`echo $(( $oct3 & $msk3 ))`;
+	ipMin[4]=`echo $((( ($oct4 & $msk4) + 1 )))`;
+}
+
+MaxHost() {
+	ipMax[1]=$(($oct1+(255-($oct1 | $msk1))));
+	ipMax[2]=$(($oct2+(255-($oct2 | $msk2))));
+	ipMax[3]=$(($oct3+(255-($oct3 | $msk3))));
+	ipMax[4]=$(($oct4+(254-($oct4 | $msk4))));
+}
+
+HostCounts() {
+	if [[ $mask == "None" ]]; then
+		return 1;
+	fi
+	msk1=`echo "$mask" | awk -F \. '{ printf $1 }'`;
+	msk2=`echo "$mask" | awk -F \. '{ printf $2 }'`;
+	msk3=`echo "$mask" | awk -F \. '{ printf $3 }'`;
+	msk4=`echo "$mask" | awk -F \. '{ printf $4 }'`;
+	hs=$(((256-$msk1)*(256-$msk2)*(256-$msk3)*(256-$msk4)-2));
+}
+
+ChooseIntreface() {
 	clear;
-	echo "Set IP";
-	echo;
-	echo -n "Enter IP: ";
-	read ip;
-	CheckIp $ip;
-	if [[ $? -ne 0 ]]; then
-		echo "Wrong ip address";
+	countFace=`ifconfig | grep -v lo | grep "Link encap" | wc -l`;
+	if [[ $countFace -gt 1 ]]; then
+		listFace=`ifconfig | grep -v lo | grep "Link encap" | awk '{ print $1 }'`;
+		number=1;
+		echo "№ IFace IP Mask" | awk '{ printf( " %2s ||%12s ||%15s ||%15s\n", $1, $2, $3, $4 ) }'
+		for i in $listFace
+		do
+			name[$number]=$i;
+			ip[$number]=`ifconfig $i | grep "inet addr" | awk '{ print $2 }' | awk -F: '{ print $2 }'`;
+			mask[$number]=`ifconfig $i | grep "inet addr" | awk '{ print $4 }' | awk -F: '{ print $2 }'`;
+			echo "$number ${name[$number]} ${ip[$number]} ${mask[$number]}" | awk '{ printf( "%2d ||%12s ||%15s ||%15s\n", $1, $2, $3, $4 ) }';
+			number=$(($number+1));
+		done
+		echo;
+		echo -n "Choose interface: ";
+		Input 1 $countFace;
+		faceNum=$?;
+		ip=${ip[$faceNum]};
+		mask=${mask[$faceNum]};
+	elif [[ $countFace -eq 1 ]]; then
+		face=`ifconfig | grep -v lo | grep "Link encap" | awk '{ print $1 }'`;
+		ip[$number]=`ifconfig $face | grep "inet addr" | awk '{ print $2 }' | awk -F: '{ print $2 }'`;
+		mask[$number]=`ifconfig $face | grep "inet addr" | awk '{ print $4 }' | awk -F: '{ print $2 }'`;
+	else
 		ip="None";
-		echo -n "Press any key to continue...";
-		read;
-	fi
-}
-
-CheckIp() {
-		# Проверка на верный формат строки
-	temp=`echo $1 | egrep '^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$'`;
-	if [[ -z $temp ]]; then
-		return 1;
-	fi
-
-		# Проверка первого значения
-	temp=`echo $1 | awk -F'\.' '{ print $1 }'`;
-	if [[ $temp -lt 0 || $temp -gt 255 ]]; then
-		return 1;
-	fi
-
-		# Проверка второго значения
-	temp=`echo $1 | awk -F'\.' '{ print $2 }'`;
-	if [[ $temp -lt 0 || $temp -gt 255 ]]; then
-		return 1;
-	fi
-
-		# Проверка третьего значения
-	temp=`echo $1 | awk -F'\.' '{ print $3 }'`;
-	if [[ $temp -lt 0 || $temp -gt 255 ]]; then
-		return 1;
-	fi
-
-		# Проверка четвертого значения
-	temp=`echo $1 | awk -F'\.' '{ print $4 }'`;
-	if [[ $temp -lt 0 || $temp -gt 255 ]]; then
-		return 1;
-	fi
-
-	return 0;
-}
-
-SetMask() {
-	clear;
-	echo "Set mask";
-	echo;
-	echo -n "Enter mask: ";
-	read mask;
-	CheckMask $mask;
-	res=$?;
-	if [[ $res -eq 1 ]]; then
-		echo "Wrong mask";
 		mask="None";
-		echo -n "Press any key to continue...";
-		read;
-	elif [[ $res -eq 2 ]]; then
-		for (( i=1; i <= 32; i++ ))
-		do
-			if [[ $i -le $mask ]]; then
-				mas[$i]=1;
-			else
-				mas[$i]=0;
-			fi
-		done
-		BinToDec;
-		mask=$address;
-	fi
-}
-
-CheckMask() {
-	temp1=`echo $1 | egrep '^[0-9]{1,2}$'`;
-	temp2=`echo $1 | egrep '^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$'`;
-	if [[ -z $temp1 && -z $temp2 ]]; then
 		return 1;
-	fi
-
-	if [[ -n $temp1 ]]; then
-			# Проверка значения на максимальную величину
-		if [[ $temp1 -lt 0 || $temp1 -gt 32 ]]; then
-			return 1;
-		else
-			return 2;
-		fi
-	elif [[ -n $temp2 ]];then
-			# Проверка первого значения
-		temp2=`echo $1 | awk -F'\.' '{ print $1 }'`;
-		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
-			return 1;
-		fi
-
-			# Проверка второго значения
-		temp2=`echo $1 | awk -F'\.' '{ print $2 }'`;
-		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
-			return 1;
-		fi
-
-			# Проверка третьего значения
-		temp2=`echo $1 | awk -F'\.' '{ print $3 }'`;
-		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
-			return 1;
-		fi
-
-			# Проверка четвертого значения
-		temp2=`echo $1 | awk -F'\.' '{ print $4 }'`;
-		if [[ $temp2 -lt 0 || $temp2 -gt 255 ]]; then
-			return 1;
-		fi
 	fi
 	return 0;
 }
 
-BinToDec() {
-	for (( i=0; i < 4; i++ ))
-	do
-		val=0;
-		for (( j=0; j < 8; j++ ))
-		do
-			if [[ ${mas[$(( 1 + $j + ( 8 * $i ) ))]} -eq 1 ]]; then
-				case "$j" in
-					0)
-						val=$(( $val + 128 ));
-						;;
-					1)
-						val=$(( $val + 64 ));
-						;;
-					2)
-						val=$(( $val + 32 ));
-						;;
-					3)
-						val=$(( $val + 16 ));
-						;;
-					4)
-						val=$(( $val + 8 ));
-						;;
-					5)
-						val=$(( $val + 4 ));
-						;;
-					6)
-						val=$(( $val + 2 ));
-						;;
-					7)
-						val=$(( $val + 1 ));
-						;;
-				esac
-			fi
-		done
-		if [[ $i -eq 0 ]]; then
-			address=$val;
-		else
-			address=$address.$val;
-		fi
-	done
-}
-
-DecToBin() {
-	temp[0]=`echo $1 | awk -F'\.' '{print $1}'`;
-	temp[1]=`echo $1 | awk -F'\.' '{print $2}'`;
-	temp[2]=`echo $1 | awk -F'\.' '{print $3}'`;
-	temp[3]=`echo $1 | awk -F'\.' '{print $4}'`;
-	for (( i=3; i >= 0; i-- ))
-	do
-		for (( j=0; j < 8; j++ ))
-		do
-			if [[ $((${temp[$i]} % 2)) -eq 0 ]]; then
-				mas[$(( 32 - $j - ( 8 * ( 3 - $i ) ) ))]=0;
-			else
-				mas[$(( 32 - $j - ( 8 * ( 3 - $i ) ) ))]=1;
-			fi
-			temp[$i]=$((${temp[$i]} / 2));
-		done
-	done
-}
-
-Calculate() {
+FindHosts() {
 	clear;
-	echo "Calculate";
-	if [[ $ip = "None" || $mask = "None" ]]; then
-		echo "Mask or IP not set";
+	ChooseIntreface;
+	if [[ $? -ne 0 ]]; then
+		echo "Error, interface not available!";
 		read;
 		return 1;
 	fi
 
-	echo "IP        : $ip";
-	echo "Mask      : $mask";
+	HostCounts;
+	MaxHosts=$hs;
+	if [[ $MaxHosts -eq 1 ]]; then
+		echo "Mask not set!";
+		read;
+		return 1;
+	fi
 
-	#
-	# Получаем адресс подсети
-	#
-	DecToBin $mask;		# Переводим маску в двоичный код
-	for (( i=1; i < 33; i++ ))
+	CurrentHost=0;
+	tmpfile=.tmp.$$;
+	touch $tmpfile;
+	echo -n > $tmpfile;
+
+	MinHost;	# обязательно вызвывать первым
+	MaxHost;
+
+	for (( a = ${ipMin[1]}; a <= ${ipMax[1]}; a++ ))
 	do
-		tmpmas[$i]=${mas[$i]};
+		for (( b = ${ipMin[2]}; b <= ${ipMax[2]}; b++ ))
+		do
+			for (( c = ${ipMin[3]}; c <= ${ipMax[3]}; c++ ))
+			do
+				for (( d = ${ipMin[4]}; d <= ${ipMax[4]}; d++ ))
+				do
+					(ping -c 1 -W 1 "$a.$b.$c.$d" | grep -B 1 "1 received" | grep "ping statistics" | awk '{ print $2 }') >> $tmpfile &
+					CurrentHost=$(($CurrentHost+1));
+					echo -ne "Progress = $(($CurrentHost*100/$MaxHosts))%\r";
+				done
+			done
+		done
 	done
+	wait;
 
-	DecToBin $ip;		# Переводим IP в двоичный код
+	ipList=`cat $tmpfile | sort`;
+	rm $tmpfile;
+	available=0;
 
-	for (( i=1; i < 33; i++ ))	# Делаем побитовое умножение маски и IP
+	clear;
+	echo "IP HWaddr" | awk '{ printf( "%16s %18s\n", $1, $2 ) }';
+	for i in $ipList
 	do
-		if [[ ${tmpmas[$i]} -eq 0 || ${mas[$i]} -eq 0 ]]; then
-			mas[$i]=0;
-		else
-			mas[$i]=1;
+		if [[ $i = $ip ]]; then
+			continue;
 		fi
+		available=$(($available+1));
+		masIP[$available]=$i;
+		masMAC[$available]=`arp -a $i | awk '{ print $4 }'`;
+		echo "${masIP[$available]} ${masMAC[$available]}" | awk '{ printf( "%16s %18s\n", $1, $2 ) }';
 	done
-	BinToDec;
-	echo "Network   : $address";
 
-	mas[32]=1;	# Вычисляем минимального хоста
-	BinToDec;
-	minHost=$address;
-
-	#
-	# Получаем broadcast
-	#
-	DecToBin $ip;		# Переводим IP в двоичный код
-	for (( i=1; i < 33; i++ ))
-	do
-		if [[ ${tmpmas[$i]} -eq 0 ]]; then
-			mas[$i]=1;
-		fi
-	done
-	BinToDec;
-	echo "Broadcast : $address";
-
-	#
-	# Получаем минимального и максимального хоста
-	#
-	echo "Min host  : $minHost";
-	mas[32]=0;	# Вычисляем максимаьлного хоста
-	BinToDec;
-	echo "Max host  : $address";
-
-	echo -n "Press any key to continue...";
+	echo -n "Press any key...";
 	read;
+	return 0;
+}
+
+ShowPrevHosts() {
+	clear;
+	echo "IP HWaddr" | awk '{ printf( "%16s %18s\n", $1, $2 ) }';
+	for (( i = 1; i <= $available; i++ ))
+	do
+		echo "${masIP[$i]} ${masMAC[$i]}" | awk '{ printf( "%16s %18s\n", $1, $2 ) }';
+	done
+
+	echo -n "Press any key...";
+	read;
+	return 0;
 }
 
 isDone=0;
@@ -286,15 +185,12 @@ while [[ $isDone -ne 1 ]]; do
 	Menu;
 	case "$?" in
 		1)
-			SetIp;
+			FindHosts;
 			;;
 		2)
-			SetMask;
+			ShowPrevHosts;
 			;;
 		3)
-			Calculate;
-			;;
-		4)
 			isDone=1
 			clear;
 			;;
